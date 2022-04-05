6d392e6a04e30c620697aab3b804c68f544743df
lakshmiselvaraj01 <lakshmiselvin67@gmail.com>
2022-04-05 23:09:15 +0530

    sucess

---
+++ src/org/demo/Sample.java
@@ -0,0 +1,18 @@
+package org.demo;
+
+public class Sample {
+	
+
+
+	public static void main(String[] args) {
+		System.out.println("employee name= lakshmi");
+
+		System.out.println("employee Id=1617");
+
+		
+		
+	}
+	
+	
+	
+}
